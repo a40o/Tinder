01a31f79054b5957a2541e94d7f48f97b999d919
Angel <a4k0o@protonmail.com>
2023-01-30 14:13:11 +0200

    genders

---
+++ src/main/java/com/volasoftware/tinder/model/Gender.java
@@ -0,0 +1,15 @@
+package com.volasoftware.tinder.model;
+
+public enum Gender {
+    MALE("male","resources/male.png"),
+    FEMALE("female","resources/female.png"),
+    OTHER("other","resources/other.png");
+
+    public final String label;
+    public final String imagePath;
+
+    private Gender(String label,String imagePath){
+        this.label = label;
+        this.imagePath = imagePath;
+    }
+}
