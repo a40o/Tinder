da8e2c87dc0c017c09b824c257cc45b89e165f1a
Angel <a4k0o@protonmail.com>
2023-03-27 13:30:05 +0300

    Created & modified acc dates && test values

--- src/main/resources/db/migration/V001__Create_user_table.sql
+++ src/main/resources/db/migration/V001__Create_user_table.sql
@@ -1,8 +1,12 @@
+SELECT CURRENT_TIMESTAMP;
+
 CREATE TABLE USER(
     ID int not null AUTO_INCREMENT PRIMARY KEY,
     FIRST_NAME varchar(50) not null,
     LAST_NAME varchar(50) not null,
     EMAIL varchar(50) not null,
     PASSWORD varchar(50) not null,
-    GENDER varchar(50) not null
+    GENDER varchar(50) not null,
+    CREATED_DATE DATETIME DEFAULT CURRENT_TIMESTAMP,
+    LAST_MODIFIED DATETIME DEFAULT CURRENT_TIMESTAMP
 );
---
+++ src/main/resources/db/migration/V002__Add_Credentials.sql
@@ -0,0 +1,13 @@
+insert into USER(
+FIRST_NAME,
+LAST_NAME,
+EMAIL,
+PASSWORD,
+GENDER
+) values(
+'Kristin',
+'Petkov',
+'da@abv.bg',
+'123',
+'female'
+);
